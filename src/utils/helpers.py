5d3d0dd950ebd2a17c8f39fcdb42c2a6daa55610
Rolando Abarza <rolando.abarza@datawalt.cl>
2025-07-23 17:24:43 -0400

    feat: schedule results builder

--- src/utils/helpers.py
+++ src/utils/helpers.py
@@ -10,3 +10,23 @@ def add_day_hour_columns(schedule_df: pd.DataFrame, H_daily_hours: int = 8):
     df_human["end_day"] = (df_human["end_time_hours"] // H_daily_hours).astype(int)
     df_human["end_hour_of_day"] = (df_human["end_time_hours"] % H_daily_hours).round(2)
     return df_human
+
+
+def build_schedule_results(jobs_data, all_tasks, solver, time_scale):
+    results = []
+    for job_id, operations in jobs_data.items():
+        for task_id in range(len(operations)):
+            start_var, end_var, _, machine = all_tasks[(job_id, task_id)]
+            start_h = solver.Value(start_var) / time_scale
+            end_h = solver.Value(end_var) / time_scale
+            results.append(
+                {
+                    "job_id": job_id,
+                    "operation_index": task_id,
+                    "machine_id": machine,
+                    "start_time_hours": round(start_h, 2),
+                    "end_time_hours": round(end_h, 2),
+                    "duration_hours": round(end_h - start_h, 2),
+                }
+            )
+    return results
